c6545fc33bcc594cd3dcd12f5b59d7f1d83d05c2
Auxia <sbhathal12@gmail.com>
2022-03-13 06:10:11 +0000

    Updated and Commented project_server.c

--- project_server.c
+++ project_server.c
@@ -18,42 +18,49 @@
  * main - Iterative UDP server for TIME service
  *------------------------------------------------------------------------
  */
+/*
+PDU : Protocol Data Unit
+*/
  
-struct pdu1 {
+struct PDUFiles { // Struct to store File Data
 	char type;
 	char peerName[10];
-	char contentName[10];
+	char fileName[10];
 	struct sockaddr_in data;
 };
 
-struct pdu2 {
+struct PDUStatus { // Struct for status messages
 	char type;
 	char data[100];
 } error, ack;
  
-struct pdu1 contentList[5];
+struct PDUFiles fileList[5];
 int counter = 0;
 
-int addToList(struct pdu1 pdu){
-	printf("PDU to add:\nType: %c\nPeer Name: %s\nContent Name: %s\n", pdu.type, pdu.peerName, pdu.contentName);
+int addToList(struct PDUFiles pdu){
+	/* Adds Files to the File List as they are registered by a peer.
+	Takes a File Data Unit as input, Returns 0 for success and -1 on failure */
+	printf("PDU to add:\nType: %c\nPeer Name: %s\nFile Name: %s\n", pdu.type, pdu.peerName, pdu.fileName);
 	printf("Port: %u\n", pdu.data.sin_port);
 	for(int i = 0; i <= counter; i++){
-		if(strcmp(contentList[i].peerName, pdu.peerName) == 0 && strcmp(contentList[i].contentName, pdu.contentName) == 0){
+		if(strcmp(fileList[i].peerName, pdu.peerName) == 0 && strcmp(fileList[i].fileName, pdu.fileName) == 0){
 			printf("Error: Names are the same\n");
 			return -1;
 		}
 	}
-	contentList[counter] = pdu;
+	fileList[counter] = pdu;
 	counter++;
 	return 0;
 }
 
-int removeFromList(struct pdu1 pdu){
+int removeFromList(struct PDUFiles pdu){
+	/*Removes Files from the File List as they are unregistered by the peers
+	Takes a File Data Unit as input, returns 0 for success and -1 for failure*/
 	for(int i = 0; i < counter; i++){
-		if(strcmp(contentList[i].peerName, pdu.peerName) == 0 && strcmp(contentList[i].contentName, pdu.contentName) == 0){
-			printf("PDU to remove: \nPeer Name: %s\nContent Name: %s\n", contentList[i].peerName, contentList[i].contentName);
+		if(strcmp(fileList[i].peerName, pdu.peerName) == 0 && strcmp(fileList[i].fileName, pdu.fileName) == 0){
+			printf("PDU to remove: \nPeer Name: %s\nFile Name: %s\n", fileList[i].peerName, fileList[i].fileName);
 			for(i; i <= counter; i++){
-				contentList[i] = contentList[i+1];
+				fileList[i] = fileList[i+1];
 			}
 			counter--;
 			return 0;
@@ -62,14 +69,16 @@ int removeFromList(struct pdu1 pdu){
 	return -1;
 }
 
-int searchList(struct pdu1 *pdu){
-	printf("Searching for PDU:\nContent Name: %s\n", pdu->contentName);
-	for(int i = counter-1; i >= 0; i--){
-		if(strcmp(contentList[i].contentName, pdu->contentName) == 0){
-			printf("Match: %s = %s\n", contentList[i].contentName, pdu->contentName);
-			strcpy(pdu->peerName, contentList[i].peerName);
-			strcpy(pdu->contentName, contentList[i].contentName);
-			pdu->data = contentList[i].data;
+int searchList(struct PDUFiles *pdu){
+	/*Searches for the filename that is input by a peer in the file list.
+	Prints if it finds a match and exits with code 0 on succes, -1 on failure*/
+	printf("Searching for PDU:\nFile Name: %s\n", pdu->fileName);
+	for(int i = counter-1; i >= 0; i--){ // Loops till it finds file or exits w/ -1
+		if(strcmp(fileList[i].fileName, pdu->fileName) == 0){
+			printf("Match: %s = %s\n", fileList[i].fileName, pdu->fileName);
+			strcpy(pdu->peerName, fileList[i].peerName);
+			strcpy(pdu->fileName, fileList[i].fileName);
+			pdu->data = fileList[i].data;
 			return 0;
 		}
 	}
@@ -77,15 +86,15 @@ int searchList(struct pdu1 *pdu){
 }
 
 void List(char* list){
-	for(int i = 0; i < counter; i++){ // loop through content
+	for(int i = 0; i < counter; i++){ // loop through File
 		int j = 0;
 		for(j = 0; j < i; j++){ // loop through unique names
-			if(strcmp(contentList[i].contentName, contentList[j].contentName) == 0){
+			if(strcmp(fileList[i].fileName, fileList[j].fileName) == 0){
 				break;
 			}
 		}
 		if(i == j){
-			strcat(list, contentList[i].contentName);
+			strcat(list, fileList[i].fileName);
 			strcat(list, "\n");
 		}
 	}
@@ -95,15 +104,14 @@ void List(char* list){
 int
 main(int argc, char *argv[])
 {
-	struct  sockaddr_in fsin;	/* the from address of a client	*/
-	//char	buf[100];		/* "input" buffer; any size > 0	*/
-	char    *pts, *filename;
-	int	sock;			/* server socket		*/
-	time_t	now;			/* current time			*/
-	int	alen;			/* from-address length		*/
-	struct  sockaddr_in sin; /* an Internet endpoint address         */
-        int     s, type;        /* socket descriptor and socket type    */
-	int 	port=3000;
+	struct  sockaddr_in fromAddr;	/* the from address of a client	*/
+	//char	buf[100];			/* "input" buffer; any size > 0	*/
+	char *pts, *filename;
+	time_t now;			/* current time			*/
+	int	addrLen;			/* from-address length		*/
+	struct sockaddr_in servAddr; /* an Internet endpoint address         */
+    int servSock, type;        /* socket descriptor and socket type    */
+	int port=3000;
 	int i, file;
 	struct stat size;
 	
@@ -116,25 +124,25 @@ main(int argc, char *argv[])
 			port = atoi(argv[1]);
 			break;
 		default:
-			fprintf(stderr, "Usage: %s [port]\n", argv[0]);
+			fprintf(stderr, "Usage: %servSock [port]\n", argv[0]);
 			exit(1);
 	}
 
-        memset(&sin, 0, sizeof(sin));
-        sin.sin_family = AF_INET;
-        sin.sin_addr.s_addr = INADDR_ANY;
-        sin.sin_port = htons(port);
+        memset(&servAddr, 0, sizeof(servAddr));
+        servAddr.sin_family = AF_INET;
+        servAddr.sin_addr.s_addr = INADDR_ANY;
+        servAddr.sin_port = htons(port);
                                                                                                  
-    /* Allocate a socket */
-        s = socket(AF_INET, SOCK_DGRAM, 0);
-        if (s < 0)
+    /* Create socket */
+        servSock = socket(AF_INET, SOCK_DGRAM, 0);
+        if (servSock < 0)
 		fprintf(stderr, "can't create socket\n");
                                                                                 
     /* Bind the socket */
-        if (bind(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
+        if (bind(servSock, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
 		fprintf(stderr, "can't bind to %d port\n",port);
-        listen(s, 5);	
-	alen = sizeof(fsin);
+        listen(servSock, 5);	
+		addrLen = sizeof(fromAddr);
 
 	
 	error.type = 'E';
@@ -146,53 +154,56 @@ main(int argc, char *argv[])
 	strcpy(error.data, errmsg);
 	
 	while (1) {
-		struct pdu1 spdu, spdu2, buf;
-		struct pdu2 opdu;
+		struct PDUFiles spdu, spdu2, buf;
+		struct PDUStatus opdu;
 		
-		// Recive pdu
-		printf("Receiving PDU...\n");
-		if (recvfrom(s, &buf, sizeof(buf), 0,	(struct sockaddr *)&fsin, &alen) < 0){
+		// Receive pdu
+		printf("Waiting for PDU...\n");
+		if (recvfrom(servSock, &buf, sizeof(buf), 0, (struct sockaddr *)&fromAddr, &addrLen) < 0){
 			fprintf(stderr, "recvfrom error\n");
 			exit(1);
 		}
 		
 		switch(buf.type){
 		case 'R':
+			// Register Files to the File List
 			printf("Got type R\n");
 			if(addToList(buf) < 0){
 				fprintf(stderr, "Error: Name already exists\n");
-				sendto(s, &error, sizeof(error), 0, (struct sockaddr *)&fsin, sizeof(fsin));
+				sendto(servSock, &error, sizeof(error), 0, (struct sockaddr *)&fromAddr, sizeof(fromAddr));
 				break;
 			}
-			sendto(s, &ack, sizeof(ack), 0, (struct sockaddr *)&fsin, sizeof(fsin));
+			sendto(servSock, &ack, sizeof(ack), 0, (struct sockaddr *)&fromAddr, sizeof(fromAddr));
 			break;
-		case 'S':
-			printf("Got type S\n");
-			spdu.type = 'S';
+		case 'L':
+			printf("Got type L\n");
+			spdu.type = 'L';
 			if(searchList(&buf) < 0){
-				fprintf(stderr, "Error: Content name not found\n");
-				sendto(s, &error, sizeof(error), 0, (struct sockaddr *)&fsin, sizeof(fsin));
+				fprintf(stderr, "Error: File name not found\n");
+				sendto(servSock, &error, sizeof(error), 0, (struct sockaddr *)&fromAddr, sizeof(fromAddr));
 				break;
 			}
 			// buf found in List
 			spdu = buf;
-			printf("Sending content's address...\n");
-			sendto(s, &spdu, sizeof(spdu), 0, (struct sockaddr *)&fsin, sizeof(fsin));
+			printf("Sending File'servSock address...\n");
+			sendto(servSock, &spdu, sizeof(spdu), 0, (struct sockaddr *)&fromAddr, sizeof(fromAddr));
 			break;
 		case 'T':
+			// Deregistering Files from File List
 			printf("Got type T\n");
 			if(removeFromList(buf) < 0){
 				fprintf(stderr, "Error: Name does not exist\n");
-				sendto(s, &error, sizeof(error), 0, (struct sockaddr *)&fsin, sizeof(fsin));
+				sendto(servSock, &error, sizeof(error), 0, (struct sockaddr *)&fromAddr, sizeof(fromAddr));
 				break;
 			}
-			sendto(s, &ack, sizeof(ack), 0, (struct sockaddr *)&fsin, sizeof(fsin));
+			sendto(servSock, &ack, sizeof(ack), 0, (struct sockaddr *)&fromAddr, sizeof(fromAddr));
 			break;
 		case 'O':
+			// Listing all files available online
 			printf("Got type O\n");
 			if(counter == 0){
 				printf("List is empty\n");
-				sendto(s, &error, sizeof(error), 0, (struct sockaddr *)&fsin, sizeof(fsin));
+				sendto(servSock, &error, sizeof(error), 0, (struct sockaddr *)&fromAddr, sizeof(fromAddr));
 				break;
 			}
 			opdu.type = 'O';
@@ -201,7 +212,7 @@ main(int argc, char *argv[])
 			List(&list);
 			strcpy(opdu.data, list);
 			printf("%s\n", opdu.data);
-			if(sendto(s, &opdu, sizeof(opdu), 0, (struct sockaddr *)&fsin, sizeof(fsin)) < 0){
+			if(sendto(servSock, &opdu, sizeof(opdu), 0, (struct sockaddr *)&fromAddr, sizeof(fromAddr)) < 0){
 				printf("O: send to error\n");
 				exit(1);
 			}
@@ -209,7 +220,7 @@ main(int argc, char *argv[])
 			break;
 		default:
 			fprintf(stderr, "Error: No match\n");
-			sendto(s, &error, sizeof(error), 0, (struct sockaddr *)&fsin, sizeof(fsin));
+			sendto(servSock, &error, sizeof(error), 0, (struct sockaddr *)&fromAddr, sizeof(fromAddr));
 			exit(1);
 		}
 		printf("\n");
